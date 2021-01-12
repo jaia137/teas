3c55795631b9685b32e97367a2ec14e959a2dcc0
jaia137 <schlaukeit@gmail.com>
2021-01-12 18:48:26 +0100

    Init ERP script

--- src/matlab/erp/TEAS_mfp_erp_v1.m
+++ src/matlab/erp/TEAS_mfp_erp_v1.m
@@ -44,10 +44,45 @@ ptb_cfg.internal_config.final_resolution = [1980 1024];
 ptb.setup_audio;
 ptb.setup_trigger;
 
+%% get tinnitus frequency
+
+tf      = inputdlg('Individuelle Tinnitusfrequenz:',...
+                   'Bitte eingeben',[1 50]);
+tin_freq = str2num(tf{:});  
+
+%% get sensation levels
+
+prompt = {'Sensation level 1000 Hz:','Sensation level 1000 Hz:','Sensation level Tinnitus:'};
+dlg_title = 'Input';
+num_lines = 1;
+answer = inputdlg(prompt,dlg_title,num_lines);
+
+sl_all = answer;
+
+sl_1000 = str2num(answer{1});
+sl_5000 = str2num(answer{2});
+sl_tin = str2num(answer{3});
+
+
+
+%% create stimuli, standard and deviants
+
+% standard
+s_1000_st_10 = o_ptb.stimuli.auditory.Sine(1000, 0.075);	
+s_1000_st_10.apply_cos_ramp(0.005);
+s_1000_st_10.db = -100+sl_1000+70; %check dynamic range
+
+% duration
+s_1000_dur_up_15 = s_1000_st_10;
+s_1000_dur_up_15.db = s_1000_dur_up_15.db + 10;
+
+s_1000_dur_dwn_15 = s_1000_st_10;
+s_1000_dur_dwn_15.db = s_1000_dur_dwn_15.db - 10;
+
 
 %% create a sine wave and make a sound object, add ramps
 
-
+% class +o_ptb.+stimuli.+auditory.Sine(freq,duration)
 
 s_rate = 44100;
 freq = 1000;
@@ -59,7 +94,7 @@ sin_sound = o_ptb.stimuli.auditory.FromMatrix(sound_data, s_rate);
 
 %% play it
 
-ptb.prepare_audio(sin_sound);
+ptb.prepare_audio(S_1000_st_10);
 ptb.schedule_audio;
 ptb.play_without_flip;
 
