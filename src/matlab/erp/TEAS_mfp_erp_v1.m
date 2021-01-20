4939823f31dff5e1fd627a6579b10aae2c7c5742
jaia137 <schlaukeit@gmail.com>
2021-01-20 22:39:09 +0100

    Update TEAS_mfp_erp_v1.m
    rubikon audio system, trigger system crossed
    -sample precise triggers at 2048 hz, optimise later
    -finish stimuli and block presentation
    -optimise (priorities) on PC level, PTB

--- src/matlab/erp/TEAS_mfp_erp_v1.m
+++ src/matlab/erp/TEAS_mfp_erp_v1.m
@@ -5,7 +5,7 @@
 % University of Zurich
 % email address: schlaukeit@gmail.com  
 % Website: 
-% December 2020; Last revision: 12-Jan-2021
+% December 2020; Last revision: 18-Jan-2021
 
 %% clear, init
 
@@ -52,7 +52,7 @@ tin_freq = str2num(tf{:});
 
 %% get sensation levels
 
-prompt = {'Sensation level 1000 Hz:','Sensation level 1000 Hz:','Sensation level Tinnitus:'};
+prompt = {'Sensation level 1000 Hz:','Sensation level 5000 Hz:','Sensation level Tinnitus:'};
 dlg_title = 'Input';
 num_lines = 1;
 answer = inputdlg(prompt,dlg_title,num_lines);
@@ -66,18 +66,54 @@ sl_tin = str2num(answer{3});
 
 
 %% create stimuli, standard and deviants
+%most critical: check dynamic range and loudness ranges!
+%also careful with one shot scripting, redo var names
+
+% constants
+f_1000 = 1000; %maybe obsolete...
+f_5000 = 5000;
+dur = 0.075; %stimulus duration
+std_db = -100+sl_1000+60; %standard SL + 60dB
+rmp = 0.005; %standard ramp
 
 % standard
-s_1000_st_10 = o_ptb.stimuli.auditory.Sine(1000, 0.075);	
-s_1000_st_10.apply_cos_ramp(0.005);
-s_1000_st_10.db = -100+sl_1000+70; %check dynamic range
+s_1000_st_10 = o_ptb.stimuli.auditory.Sine(1000, dur);	
+s_1000_st_10.db = std_db; %check dynamic range
+s_1000_st_10.apply_cos_ramp(rmp);
 
-% duration
-s_1000_dur_up_15 = s_1000_st_10;
-s_1000_dur_up_15.db = s_1000_dur_up_15.db + 10;
+% loudness
+s_1000_loud_up_13 = o_ptb.stimuli.auditory.Sine(1000, dur);
+s_1000_loud_up_13.db = std_db + 10;
+s_1000_loud_up_13.apply_cos_ramp(rmp);
 
-s_1000_dur_dwn_15 = s_1000_st_10;
-s_1000_dur_dwn_15.db = s_1000_dur_dwn_15.db - 10;
+s_1000_loud_dwn_14 = o_ptb.stimuli.auditory.Sine(1000, dur);
+s_1000_loud_dwn_14.db = std_db - 10;
+s_1000_loud_dwn_14.apply_cos_ramp(rmp);
+
+% freq
+s_1000_freq_up_11 = o_ptb.stimuli.auditory.Sine(f_1000+0.1*f_1000, dur);
+s_1000_freq_up_11.db = std_db;
+s_1000_freq_up_11.apply_cos_ramp(rmp);
+
+s_1000_freq_down_12 = o_ptb.stimuli.auditory.Sine(f_1000-0.1*f_1000, dur);
+s_1000_freq_down_12.db = std_db;
+s_1000_freq_down_12.apply_cos_ramp(rmp);
+
+%duration
+s_1000_dur_up_15 = o_ptb.stimuli.auditory.Sine(1000, dur+0.015);
+s_1000_dur_up_15.db = std_db;
+s_1000_dur_up_15.apply_cos_ramp(rmp);
+
+s_1000_dur_dwn_16 = o_ptb.stimuli.auditory.Sine(1000, dur-0.015);
+s_1000_dur_dwn_16.db = std_db;
+s_1000_dur_dwn_16.apply_cos_ramp(rmp);
+
+%location
+
+
+
+%gap
+
 
 
 %% create a sine wave and make a sound object, add ramps
@@ -94,7 +130,7 @@ sin_sound = o_ptb.stimuli.auditory.FromMatrix(sound_data, s_rate);
 
 %% play it
 
-ptb.prepare_audio(S_1000_st_10);
+ptb.prepare_audio(s_1000_st_10);
 ptb.schedule_audio;
 ptb.play_without_flip;
 
