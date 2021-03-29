a26a3cef976096995598a1717fd69d7158dd362f
jaia137 <schlaukeit@gmail.com>
2021-03-29 18:28:45 +0200

    Add electrode file, layout; Init topo and source level analysis
    that leadfield takes time to calculate...

--- src/matlab/erp/teas_erp_analysis_v1_mac.m
+++ src/matlab/erp/teas_erp_analysis_v1_mac.m
@@ -1,4 +1,4 @@
-%% PATHS
+%% PATHS, INIT
 
 % cd(m1path);
 
@@ -7,6 +7,7 @@ cd( '/Users/shanti/Desktop');
 % cd((m1dir(i));    
 
 ft_defaults
+
 
 %% FILENAME
 
@@ -22,70 +23,8 @@ event = event(sel);
 
 plot([event.sample], [event.value], '.')
 
-%% TRIALS and LOAD/PREPROC
 
-cfg = [];
-cfg.dataset = dataset;               
-% cfg.overlap = 1;
-cfg.trialdef.eventtype = 'STATUS';
-cfg.trialdef.pre  = 0;
-cfg.trialdef.post = 0.5;
-
-cfg = ft_definetrial(cfg);                  
-
-trl = cfg.trl;
-
-trl = trl(76:3765,1:4); %make a generic remove test trial standards function, here the paradigm has been repeatedly restarted...
-
-% 
-
-% cfg.trialdef.eventtype	= 'STATUS';
-% cfg.trialdef.eventvalue = '60';
-% cfg.trialdef.pre	= 0;
-% cfg.trialdef.post	= 2;
-%cfg.continuous		= 'no'; 
-%cfg.dftfilter = 'yes';
-%cfg.dtfreq = [50 100 150];
-%cfg.detrend = 'yes';
-% cfg.bpfilter = 'yes';
-% cfg.bpfreq = [1 30];  
-% cfg.continuous		= 'yes';
-% cfg.trialfun		= 'trialfuck';  
-%cfg.channel = {'1-A1','1-A2','1-A3','1-A4','1-A5','1-A6','1-A7','1-A8','1-A9','1-A10','1-A11','1-A12','1-A13','1-A14','1-A15','1-A16','1-A17','1-A18','1-A19','1-A20','1-A21','1-A22','1-A23','1-A24','1-A25','1-A26','1-A27','1-A28','1-A29','1-A30','1-A31','1-A32','1-B1','1-B2','1-B3','1-B4','1-B5','1-B6','1-B7','1-B8','1-B9','1-B10','1-B11','1-B12','1-B13','1-B14','1-B15','1-B16','1-B17','1-B18','1-B19','1-B20','1-B21','1-B22','1-B23','1-B24','1-B25','1-B26','1-B27','1-B28','1-B29','1-B30','1-B31','1-B32','STATUS'};
-% cfg.layout		= 'biosemi_testin.lay';
-% cfg 			= ft_definetrial(cfg);
-
-
-% cfg.hdr = ft_read_header (dataset);
-% cfg.channel = {'1-A1','1-A2','1-A3','1-A4','1-A5','1-A6','1-A7','1-A8','1-A9','1-A10','1-A11','1-A12','1-A13','1-A14','1-A15','1-A16','1-A17','1-A18','1-A19','1-A20','1-A21','1-A22','1-A23','1-A24','1-A25','1-A26','1-A27','1-A28','1-A29','1-A30','1-A31','1-A32','1-B1','1-B2','1-B3','1-B4','1-B5','1-B6','1-B7','1-B8','1-B9','1-B10','1-B11','1-B12','1-B13','1-B14','1-B15','1-B16','1-B17','1-B18','1-B19','1-B20','1-B21','1-B22','1-B23','1-B24','1-B25','1-B26','1-B27','1-B28','1-B29','1-B30','1-B31','1-B32'};
-% cfg.channel = {'all'};
-% cfg.hpfilter = 'yes';
-% cfg.hpfreq = 1;
-
-data = ft_preprocessing(cfg);
-
-%% RAW DATA INSPECT
-
-cfg =[];
-% cfg.channel = {'all'};
-% cfg.continuous 	= 'yes';
-% % cfg.blocksize 	= 2;
-% cfg.eventfile 	= event;
-cfg.viewmode 	= 'vertical';
-
-ft_databrowser(cfg, data);
-
-
-%%
-
-
-cfg = [];
-cfg.layout = 'biosemi128.lay';
-ft_layoutplot(cfg)
-
-
-
-%%
+%% LOAD, FILTER
 
 cfg = [];
 cfg.dataset = dataset;
@@ -107,92 +46,208 @@ cfg.refchannel = {'1-EXG4', '1-EXG8'};
 % cfg.demean = 'yes';
 % cfg.baselinewindow = [-0.2 0];
 
-
 data = ft_preprocessing(cfg);
 
-% cfg = [];
-% timelock = ft_timelockanalysis(cfg, data);
+% fix to somehow just have the exp trials (not pre run)
+trl = cfg.trl;
+trl = trl(76:3765,1:4); %make a generic remove test trial standards function, here the paradigm has been repeatedly restarted...
+
+%% FIX channel labels (run once, else deleted. go for if...)
+
+for k = 1 : length(data.label)
+    cellContents = data.label{k};
+    data.label{k} = cellContents(3:end);
+end
+
+for j = 1 : length(data.cfg.channel)
+    cellContents = data.cfg.channel{j};
+    data.cfg.channel{j} = cellContents(3:end);
+end
+
+%% RAW DATA INSPECT
+
+cfg =[];
+cfg.viewmode 	= 'vertical';
+
+ft_databrowser(cfg, data);
 
 
-%%
+%% PREP layout, elecs
+
+cfg      = [];
+cfg.elec = 'biosemi128_elec.elc';
+layout_eeg = ft_prepare_layout(cfg);
+
+elec = ft_read_sens('biosemi128_elec.elc');
+
+cfg = [];
+cfg.layout = layout_eeg;
+ft_layoutplot(cfg)
+
+
+%% PREP neighbours
+
+cfg = [];
+cfg.layout = layout_eeg;
+% cfg.elec   = elec;
+cfg.method = 'triangulation'; 
+cfg.feedback = 'yes';
+
+neighbours = ft_prepare_neighbours(cfg);
+
+
+%% PREPROC 1: Channels
 
 cfg        = [];
-cfg.trials = find(data.trialinfo(:,1)==10);
+cfg.metric = 'zvalue';  
+cfg.method = 'summary'; 
+
+data_badchan       = ft_rejectvisual(cfg, data);
+
+removed_channels = setdiff(data.label,data_badchan.cfg.channel);
+
+
+%% INTERPOLATE badchan
+
+cfg = [];
+cfg.method         =  'weighted'; %'weighted', 'average', 'spline', 'slap' or 'nan' (default = 'weighted')
+cfg.badchannel     = removed_channels;
+cfg.elec           = elec;
+cfg.missingchannel = []; %cell-array, see FT_CHANNELSELECTION for details
+cfg.neighbours     = neighbours; %neighbourhood structure, see also FT_PREPARE_NEIGHBOURS
+
+data_interp = ft_channelrepair(cfg, data_badchan);
+
+
+%% PREPROC 2: Trials
+
+cfg        = [];
+cfg.metric = 'zvalue';  
+cfg.method = 'summary'; 
+
+data_clean       = ft_rejectvisual(cfg, data_interp);
+
+
+%% SAVE clean data
+
+save ('data_clean.mat','data_clean','-v7.3');
+
+
+%% MAKE ERPs
+
+cfg        = [];
+cfg.trials = find(data_clean.trialinfo(:,1)==10);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_st = ft_timelockanalysis(cfg, data);
+avg_500_st = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==11);
+cfg.trials = find(data_clean.trialinfo(:,1)==11);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_fu = ft_timelockanalysis(cfg, data);
+avg_500_fu = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==12);
+cfg.trials = find(data_clean.trialinfo(:,1)==12);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_fd = ft_timelockanalysis(cfg, data);
+avg_500_fd = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==13);
+cfg.trials = find(data_clean.trialinfo(:,1)==13);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_lu = ft_timelockanalysis(cfg, data);
+avg_500_lu = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==14);
+cfg.trials = find(data_clean.trialinfo(:,1)==14);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_ld = ft_timelockanalysis(cfg, data);
+avg_500_ld = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==15);
+cfg.trials = find(data_clean.trialinfo(:,1)==15);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_ll = ft_timelockanalysis(cfg, data);
+avg_500_ll = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==16);
+cfg.trials = find(data_clean.trialinfo(:,1)==16);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_lr = ft_timelockanalysis(cfg, data);
+avg_500_lr = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==17);
+cfg.trials = find(data_clean.trialinfo(:,1)==17);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_dr = ft_timelockanalysis(cfg, data);
+avg_500_dr = ft_timelockanalysis(cfg, data_clean);
 
-cfg.trials = find(data.trialinfo(:,1)==18);
+cfg.trials = find(data_clean.trialinfo(:,1)==18);
 cfg.preproc.demean = 'yes';
 cfg.preproc.baselinewindow = [-0.1 0];
-avg_500_gp = ft_timelockanalysis(cfg, data);
+avg_500_gp = ft_timelockanalysis(cfg, data_clean);
 
 
-
-%%
-
-% create an EEG channel layout on-the-fly and visualise the eeg data
-cfg      = [];
-cfg.layout = 'biosemi128.lay';
-layout_eeg = ft_prepare_layout(cfg);
+%% MULTIPLOT
 
 cfg        = [];
 cfg.layout = layout_eeg;
-cfg.channel = {'all', '-SCALE', '-COMNT'};
-figure; ft_multiplotER(cfg, avg_500_st, avg_500_fu, avg_500_fd, avg_500_lu, avg_500_ld, avg_500_ll, avg_500_lr, avg_500_dr, avg_500_gp);
+cfg.channel = {'all', '-COMNT','-EXG1', '-EXG2', '-EXG3', '-EXG4', '-EXG5', '-EXG6', '-EXG7', '-EXG8', '-STATUS'};
+
+figure; ft_multiplotER(cfg, avg_500_st, avg_500_fu, avg_500_fd, avg_500_lu, avg_500_ld, avg_500_ll, avg_500_lr, avg_500_dr, avg_500_gp)
 
 
-%%
+%% SINGLEPLOT
+% Cz
 
 cfg= [];
-cfg.channel = {'1-A1'};
-
+cfg.channel = {'A1'};
 
 ft_singleplotER(cfg, avg_500_st, avg_500_fu, avg_500_fd, avg_500_lu, avg_500_ld, avg_500_ll, avg_500_lr, avg_500_dr, avg_500_gp);
 
 
+%% TOPOPLOT
+
+cfg = [];
+cfg.xlim = [0.0 0.2];
+% cfg.zlim = [0 6e-14];
+cfg.layout = layout_eeg;
+% cfg.parameter = 'individual'; % the default 'avg' is not present in the data
+figure; ft_topoplotER(cfg,avg_500_st, avg_500_fu, avg_500_fd, avg_500_lu, avg_500_ld, avg_500_ll, avg_500_lr, avg_500_dr, avg_500_gp); colorbar
 
 
+%% SOURCE
+
+cfg                 = [];
+% cfg.channel         = data_clean.label; % ensure that rejected sensors are not present
+cfg.elec            = elec;
+cfg.headmodel       = vol;
+cfg.reducerank = 3; % default for MEG is 2, for EEG is 3
+cfg.resolution = 1;   % use a 3-D grid with a 1 cm resolution
+cfg.unit       = 'cm';
+% cfg.tight      = 'yes';
+cfg.normalize = 'yes';
 
 
+[grid] = ft_prepare_leadfield(cfg);
+% [grid] = ft_convert_units(grid, 'mm');
 
 
+%% PLOT headmodel, grid, sensor
+%PN: aufpassen, stimmen die units (mm/cm)
 
+cfg = [];
+figure;
+ft_plot_vol(vol, 'edgecolor', 'black','facealpha', 0.1, 'unit', 'mm'); %alpha 0.4;
+ft_plot_mesh(grid.pos(grid.inside,:));
+ft_plot_sens(elec, 'style', 'g*');
+    
+    
+% head surface (scalp)
+ft_plot_mesh(vol.bnd(1), 'edgecolor','none','facealpha',0.8,'facecolor',[0.6 0.6 0.8]); 
+hold on;
+% electrodes
+ft_plot_sens(elec,'style', 'g*');
 
+    
+%% REALIGN if needed
 
-
+cfg           = [];
+cfg.method    = 'interactive';
+cfg.elec      = elec;
+cfg.headshape = vol.bnd(1);
+elec_aligned  = ft_electroderealign(cfg);
